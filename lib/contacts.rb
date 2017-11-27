90d702e5557ea7eef3f51f0bba7bee4c88c0eaf5
Chris Gullickson <cgullickson5@gmail.com>
2017-11-27 18:02:12 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,7 +4,7 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
+  #     email: "jon_snow@thewall.we",
   #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
@@ -17,7 +17,9 @@ require 'pry'
 
 
 def remove_strawberry(contacts)
-    contacts.each do |person, contact_details_hash|
-    binding.pry
+  contacts.each do |person, contact_details_hash|
+    contact_details_hash.each do |attribute, data|
+      binding.pry
+    end
   end
 end
